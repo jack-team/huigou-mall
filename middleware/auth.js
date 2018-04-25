2179344afe42ceaa0deefd437007a6a166ad6042
jiangtao <jiangtao@wesai.com>
2018-04-25 20:43:12 +0800

    T

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,22 @@
 const auth = async (ctx, next) => {
-    if (!ctx.isLogin) {
+    const { methods , isLogin } = ctx;
+    if (!isLogin) {
         return ctx.body = {
             code: 403,
             message: `请先登录`
         }
     }
+
+    const user = methods.baseUser();
+
+    //如果不是管理员
+    if(!user.isAdmin) {
+        return ctx.body = {
+            code: 699,
+            message: `不是管理员，无法访问！`
+        }
+    }
+
     return next();
 };
 
