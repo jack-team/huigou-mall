f5436466cfaab1251fe94369452738491959720e
jiangtao <jiangtao@wesai.com>
2018-07-05 18:17:22 +0800

    t

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,16 +1,26 @@
+const fs = require('fs'),
+    process = require("process");
+const staticPath = `${process.cwd()}/public`;
+
 const index = async ctx => {
-    const {methods , isLogin } = ctx;
-    const staticUrl = `http://localhost:8088/`;
+    const {
+        methods,
+        isLogin
+    } = ctx;
+    const staticUrl = ``;
     const user = methods.baseUser();
+    //获取静态路径
+    const assetPath = `${staticPath}/asset.json`;
+    const asset = fs.readFileSync(assetPath, `utf-8`);
+
     await ctx.render("admin", {
-        user:JSON.stringify({
+        user: JSON.stringify({
             ...user,
             isLogin
         }),
         static: {
             staticUrl: staticUrl,
-            css: [`css/app.css`],
-            js: [`js/common.js`, `js/app.js`]
+            ...asset
         }
     })
 };
